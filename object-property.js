fbbdad77651794740ce335ba61f7ef4e6418646e
Rakibur Rahman <rakibur5454@gmail.com>
2021-02-02 00:01:32 +0600

    ob-property

--- object-property.js
+++ object-property.js
@@ -13,9 +13,9 @@ const student = [
 // }
 // console.log(profile);
 
-const names = student.map(s1 => s1.name);
-const ids = student.map(s2 => s2.id);
-const bigger = student.filter(s3 => s3.id > 2);
-const bigger1 = student.find(s4 => s4.id > 1);
+const names = student.map(s1 => s1.name);         //[ 'Rakib', 'Tamim', 'Shakib', 'Mushfiqur' ]
+const ids = student.map(s2 => s2.id);             //[ 1, 2, 3, 4 ]
+const bigger = student.filter(s3 => s3.id > 2);   //[ { id: 3, name: 'Shakib' }, { id: 4, name: 'Mushfiqur' } ]
+const bigger1 = student.find(s4 => s4.id > 1);    //{ id: 2, name: 'Tamim' }
 console.log(bigger1);
 console.log(ids);
