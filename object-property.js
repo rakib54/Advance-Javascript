4e2fe1eea2ba99aff0a9835ceba1c8436fad3321
Rakibur Rahman <rakibur5454@gmail.com>
2021-02-01 23:57:31 +0600

    ob-property

---
+++ object-property.js
@@ -0,0 +1,21 @@
+const student = [
+    { id: 1, name: 'Rakib' },
+    { id: 2, name: 'Tamim' },
+    { id: 3, name: 'Shakib' },
+    { id: 4, name: 'Mushfiqur' }
+];
+// const profile = [];
+// for (let i = 0; i < student.length; i++) {
+//     const element = student[i];
+//     const StudentName = element.name;
+//     profile.push(StudentName);
+
+// }
+// console.log(profile);
+
+const names = student.map(s1 => s1.name);
+const ids = student.map(s2 => s2.id);
+const bigger = student.filter(s3 => s3.id > 2);
+const bigger1 = student.find(s4 => s4.id > 1);
+console.log(bigger1);
+console.log(ids);
