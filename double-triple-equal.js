fdd8b52c8b9a93f0c98ffc0e7fcffe0e8c1b467c
Rakibur Rahman <rakibur5454@gmail.com>
2021-02-01 23:06:42 +0600

    double-triple

---
+++ double-triple-equal.js
@@ -0,0 +1,19 @@
+//Program 1
+const num1 = 2;
+const num2 = "2";
+if (num1 == num2) {      //It checks only value. It returns True  
+    console.log('True')
+}
+else {
+    console.log('false')
+}
+
+//Program 2
+const num1 = 2;
+const num2 = "2";
+if (num1 === num2) {      //It checks value and data Type. It returns False  
+    console.log('True')
+}
+else {
+    console.log('false')
+}
